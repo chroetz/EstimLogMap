b2353928b3999013bc2ca406b5069d551c91e6d8
Christof Schoetz <math@christof-schoetz.de>
2024-11-29 16:08:08 +0100

    fix

--- slurm_estimGridSearch1.R
+++ slurm_estimGridSearch1.R
@@ -4,7 +4,7 @@ repIdxRanges <- lapply(1:100, \(x) c(1 + 10*(x-1), 10*x))
 
 for (repIdxRange in repIdxRanges) for (noiseType in noiseTypes) for (noiseParamsIdx in noiseParamsIdxs[[noiseType]])  {
 #for (repIdxRange in repIdxRanges[1:2]) for (noiseType in noiseTypes[1:2]) for (noiseParamsIdx in noiseParamsIdxs[[noiseType]][1:2])  {
-  command <- sprintf("sbatch --qos=short --job-name=ELMgrid --output=ELMgrid_%%j.out --error=ELMgrid_%%j.err --time=1440 --cpus-per-task=1 --wrap=\"Rscript estimGridSearch1.R noiseType='%s' noiseParamsIdx=%d repIdx=%d:%d\"", noiseType, noiseParamsIdx, repIdxRange[1], repIdxRange[2])
+  command <- sprintf("sbatch --qos=short --job-name=ELMgrid --output=_log/ELMgrid_%%j.out --error=_log/ELMgrid_%%j.err --time=1440 --cpus-per-task=1 --wrap=\"Rscript estimGridSearch1.R noiseType=\\'%s\\' noiseParamsIdx=%d repIdx=%d:%d\"", noiseType, noiseParamsIdx, repIdxRange[1], repIdxRange[2])
   cat(command, "\n")
   output <- system(command, intern = TRUE)
   cat(output, "\n")
